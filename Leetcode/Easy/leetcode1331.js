bcd2e69f2caebc1570c79eda3b599fd1a1b739ea
Santhosh Gopisetty <santhoshgopisetty4@gmail.com>
2026-07-12 22:56:32 +0530

    POD's

---
+++ Leetcode/Easy/leetcode1331.js
@@ -0,0 +1,18 @@
+var arrayRankTransform = function (arr) {
+    const sorted = [...arr];
+    sorted.sort((a, b) => a - b);
+
+    const rank = new Map();
+    let currentRank = 1;
+
+    for (let num of sorted) {
+        if (!rank.has(num)) {
+            rank.set(num, currentRank++);
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = rank.get(arr[i]);
+    }
+    return arr;
+};
